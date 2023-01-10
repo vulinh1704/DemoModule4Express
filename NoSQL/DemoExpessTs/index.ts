500e92b03963cd2562d88027a89c0324e02b82c2
vulinh1704 <truongdangvulinh1704@gmail.com>
2023-01-10 15:34:04 +0700

    session

--- NoSQL/DemoExpessTs/index.ts
+++ NoSQL/DemoExpessTs/index.ts
@@ -19,8 +19,32 @@ mongoose.connect('mongodb://localhost:27017/test').then(() => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
+// app.use(session({
+//     resave: true,
+//     saveUninitialized: true,
+//     secret: 'somesecret',
+//     cookie: { maxAge: 60000 }}));
 app.use('', router);
 
 app.listen(3000, () => {
     console.log('Server is running')
 })
+
+// app.get('/login', (req, res) => {
+//     // @ts-ignore
+//     req.session.User = {
+//         username: 'linh',
+//         password: '1'
+//     }
+//
+//     return res.status(200).json({status: 'success'})
+// })
+//
+// app.get('/get_session', (req, res) => {
+//     // @ts-ignore
+//     if(req.session.User){
+//         // @ts-ignore
+//         return res.status(200).json({status: 'success', session: req.session.User})
+//     }
+//     return res.status(200).json({status: 'error', session: 'No session'})
+// })
